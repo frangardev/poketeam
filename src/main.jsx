bb7a4b24fe60811b4a1e32ef585c77c34eed5181
Francisco García <frangardev@outlook.com>
2023-03-06 18:04:23 -0600

    add config ridux tools

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 import "./index.css";
 
-const store = createStore(pokemonReducer);
+const store =
+  createStore(pokemonReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
