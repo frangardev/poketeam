4b140932f0a44c7d789cce36fd6fa5f62399e338
Francisco García <frangardev@outlook.com>
2023-05-19 12:44:28 -0600

    add Head

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,27 @@ import { store } from '../redux/store';
 import { Provider } from "react-redux";
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import { theme } from "../src/resources/theme";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-    console.log('theme: ', theme);
     return (
-        <Provider store={store}>
-            <ChakraBaseProvider theme={theme}>
-                <Component {...pageProps} />
-            </ChakraBaseProvider>
-        </Provider>
+        <>
+            <Head>
+                <link rel="shortcut icon" href="https://i.ibb.co/vdQtj5K/pokeball.png" />
+                <title>PokeTeam</title>
+                <meta name="description" content="Crea y revisa las estadísticas de tu equipo Pokémon" />
+                <meta name="og:title" content="Crea tu quipo Pokémon" />
+                <meta name="og:description" content="Crea y revisa las estadísticas de tu equipo Pokémon" />
+                <meta name="og:image" content="https://i.ibb.co/C115ctN/poketeam.png" />
+                <meta property="og:type" content="website" />
+            </Head>
+            <Provider store={store}>
+
+                <ChakraBaseProvider theme={theme}>
+                    <Component {...pageProps} />
+                </ChakraBaseProvider>
+            </Provider>
+        </>
     );
 }
 
